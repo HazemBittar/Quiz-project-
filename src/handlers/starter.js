e4606374b39b316d50047f37b72e357aab007312
Hazem Bittar <mr.hazem@hotmail.fr>
2020-08-21 00:02:57 +0200

    Update starter.js

--- src/handlers/starter.js
+++ src/handlers/starter.js
@@ -1,20 +1,36 @@
-'use strict';
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const results = document.querySelector('.result');
 
-console.log('--- loading handler: _');
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-const _ = (event) => {
+    let score = 0;
+    const userAnswers = [
+		form.q1.value,
+		form.q2.value,
+		form.q3.value,
+		form.q4.value
+	];
 
-};
+    // check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
 
-/* handlers define user interactions
+    scrollTo(0,0);
+    results.classList.remove('d-none');
+    
+    let output = 0;
+    const timer = setInterval(() => {
+        results.querySelector('span').textContent = `${output}%`;
+        if(output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    },10);
 
-  they read user input
-    from events or from the DOM
-
-  they process user data using program logic functions
-
-  they communicate results to the user
-
-  they log any important information for developers
-
-*/
+});
